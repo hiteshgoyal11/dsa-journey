5b959bd8871beb748b4a2b4ed3fb9b60d2f0f667
hiteshgoyal01 <goyalhitesh425@gmail.com>
2026-08-18 17:48:46 +0300

    RecursionBasicspart2:  remove duplicates in string problem

--- RecursionBasicspart2.java
+++ RecursionBasicspart2.java
@@ -1,19 +1,45 @@
 public class RecursionBasicspart2 {
-    public static int tilingproblem(int n) {
-        if(n == 0 || n == 1) {
-            return 1;
-        }
-        // vertical choice
-        int fnm1 = tilingproblem(n-1);
+    // Tiling Problem
+    // public static int tilingproblem(int n) {
+    //     if(n == 0 || n == 1) {
+    //         return 1;
+    //     }
+    //     // vertical choice
+    //     int fnm1 = tilingproblem(n-1);
 
-        // horizontal choice
-        int fnm2 = tilingproblem(n-2);
+    //     // horizontal choice
+    //     int fnm2 = tilingproblem(n-2);
         
-        int totalways = fnm1 + fnm2;
-        return totalways;
+    //     int totalways = fnm1 + fnm2;
+    //     return totalways;
+    // }
+
+
+
+    // Remove Duplicates in a string
+    public static void removeDuplicates(String str, int idx, StringBuilder newstr, boolean map[]) {
+        if(idx == str.length()) {
+            System.out.println(newstr);
+            return;
+        }
+
+        char currchar = str.charAt(idx);
+        if(map[currchar - 'a'] == true) {
+            removeDuplicates(str, idx+1, newstr, map);
+        } else {
+            map[currchar - 'a'] = true;
+            removeDuplicates(str, idx+1, newstr.append(currchar), map);
+        }
     }
 
     public static void main(String args[]) {
-        System.out.println(tilingproblem(1000));
+        // Tiling Problem
+        // System.out.println(tilingproblem(1000));
+
+
+
+        // Remove Duplicates in a string
+        String str = "appnnacollege";
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
     }
 }
