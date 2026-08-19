988095d00edaad5a10ecf18b22a11c677b3504da
hiteshgoyal01 <goyalhitesh425@gmail.com>
2026-08-19 17:11:23 +0300

    Add friends pairing problem

--- RecursionBasicspart2.java
+++ RecursionBasicspart2.java
@@ -42,17 +42,17 @@ public class RecursionBasicspart2 {
 
         // choice
         // 1. single
-        // int fnm1 = friendsPairing(n-1);
+        int fnm1 = friendsPairing(n-1);
 
         // 2. pair
-        // int fnm2 = friendsPairing(n-2);
-        // int pairWays = (n-1) * fnm2;
+        int fnm2 = friendsPairing(n-2);
+        int pairWays = (n-1) * fnm2;
 
         // totWays
-        // int totWays = fnm1 + pairWays;
-        // return totWays;
+        int totWays = fnm1 + pairWays;
+        return totWays;
 
-        return friendsPairing(n-1) + (n-1) * friendsPairing(n-2);
+        // return friendsPairing(n-1) + (n-1) * friendsPairing(n-2);
     }
 
     public static void main(String args[]) {
