568c633b276877501b375cbb30396e50aec12a3a
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-04 19:49:37 +0530

    New update in RecursionPractice

--- RecursionPractice.java
+++ RecursionPractice.java
@@ -15,20 +15,31 @@ public class RecursionPractice {
     // }
 
 
-    static String digit[] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    // static String digit[] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
     // Print Digits
-    public static void printDigits(int n) {
+    // public static void printDigits(int n) {
+    //     // Base case
+    //     if(n == 0) {
+    //         return;
+    //     }
+    //     // Kaam
+    //     int lastDigit = n%10;
+    //     printDigits(n/10);
+    //     System.out.print(digit[lastDigit]+" ");
+
+
+
+    // Print length of string
+    public static int lenOfString(String str, int i, int count) {
         // Base case
-        if(n == 0) {
-            return;
+        if(i == str.length()) {
+            System.out.println(count);
+            return count;
         }
-        // Kaam
-        int lastDigit = n%10;
-        printDigits(n/10);
-        System.out.print(digit[lastDigit]+" ");
-
-
+        // kaam
+        return lenOfString(str, i+1, count+1);
     }
+    // }
     public static void main(String args[]) {
 
 
@@ -40,6 +51,12 @@ public class RecursionPractice {
 
 
         // print Digits
-        printDigits(2019);
+        // printDigits(2019);
+
+
+
+        // Print length of string
+        String str = "Apna College";
+        lenOfString(str, 0, 0);
     }
 }
