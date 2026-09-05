64afae56dca848ada0d97d96b9c91356ad270263
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-05 16:41:19 +0530

    Rotated Sorted Array problem added in DivideNConqueur

--- DivideNConqueur.java
+++ DivideNConqueur.java
@@ -47,44 +47,89 @@ public class DivideNConqueur {
     // }
 
 
-    public static void printArr(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i]+" ");
-        }
-    }
-    public static void quickSort(int arr[], int si, int ei) {
+
+    // Quick Sort
+    // public static void printArr(int arr[]) {
+    //     for(int i=0; i<arr.length; i++) {
+    //         System.out.print(arr[i]+" ");
+    //     }
+    // }
+    // public static void quickSort(int arr[], int si, int ei) {
+    //     // Base case
+    //     if(si >= ei) {
+    //         return;
+    //     }
+    //     // kaam
+    //     int pIdx = partition(arr, si, ei);
+    //     quickSort(arr, si, pIdx-1);
+    //     quickSort(arr, pIdx+1, ei);
+    // }
+    // public static int partition(int arr[], int si, int ei) {
+    //     int pivot = arr[ei];
+    //     int i = si-1;
+    //     for(int j=si; j<ei; j++) {
+    //         if(arr[j] <= pivot) {
+    //             i++;
+    //             // swap
+    //             int temp = arr[j];
+    //             arr[j] = arr[i];
+    //             arr[i] = temp;
+    //         }
+    //     }
+    //     i++;
+    //     // swap
+    //     int temp = pivot;
+    //     arr[ei] = arr[i];
+    //     arr[i] = temp;
+    //     return i;
+    // }
+
+
+
+    // Rotated Sorted Array
+    public static int search(int arr[], int tar, int si, int ei) {
         // Base case
-        if(si >= ei) {
-            return;
+        if(si > ei) {
+            return -1;
         }
-        // kaam
-        int pIdx = partition(arr, si, ei);
-        quickSort(arr, si, pIdx-1);
-        quickSort(arr, pIdx+1, ei);
-    }
-    public static int partition(int arr[], int si, int ei) {
-        int pivot = arr[ei];
-        int i = si-1;
-        for(int j=si; j<ei; j++) {
-            if(arr[j] <= pivot) {
-                i++;
-                // swap
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+        // Kaam
+        int mid = si + (ei-si)/2;
+        // case FOUND
+        if(arr[mid] == tar) {
+            return mid;
+        }
+
+        // mid on L1
+        if(arr[si] <= arr[mid]) {
+            // case a: left
+            if(arr[si] <= tar && tar <= arr[mid]) {
+                return search(arr, tar, si, mid);
+            } else {
+                return search(arr, tar, mid+1, ei);
             }
         }
-        i++;
-        // swap
-        int temp = pivot;
-        arr[ei] = arr[i];
-        arr[i] = temp;
-        return i;
+
+        // mid on L2
+        else {
+            // case c: right
+            if(arr[mid] <= tar && tar <= arr[ei]) {
+                return search(arr, tar, mid, ei);
+            } else {
+                return search(arr, tar, si, mid-1);
+            }
+        }
     }
     public static void main(String args[]) {
-        int arr[] = {6, 3, 9, 5, 2, 8};
+        // int arr[] = {6, 3, 9, 5, 2, 8};
         // mergeSort(arr, 0, arr.length-1);
-        quickSort(arr, 0, arr.length-1);
-        printArr(arr);
+        // quickSort(arr, 0, arr.length-1);
+        // printArr(arr);
+
+
+        // Rotated Sorted Array
+        int arr[] = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0; //output -> 4
+        int tarIdx = search(arr, target, 0, arr.length-1);
+        System.out.println(tarIdx);
     }
 }
