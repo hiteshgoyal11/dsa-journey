d8ffe5c18a9343bff1ec02685de74a412808c1d3
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-05 15:38:40 +0530

    Quick Sort using Recursion in DivideNConqueur

--- DivideNConqueur.java
+++ DivideNConqueur.java
@@ -1,54 +1,90 @@
 public class DivideNConqueur {
     // Merge Sort
+    // public static void printArr(int arr[]) {
+    //     for(int i=0; i<arr.length; i++) {
+    //         System.out.print(arr[i]+" ");
+    //     }
+    // }
+    // public static void mergeSort(int arr[], int si, int ei) {
+    //     // Base case
+    //     if(si >= ei) {
+    //         return;
+    //     }
+    //     // Kaam
+    //     int mid = si + (ei-si)/2;
+    //     mergeSort(arr, si, mid);
+    //     mergeSort(arr, mid+1, ei);
+    //     merge(arr, si, mid, ei);
+    // }
+
+    // public static void merge(int arr[], int si, int mid, int ei) {
+    //     int temp[] = new int[ei-si+1];
+    //     int i = si;
+    //     int j = mid+1;
+    //     int k = 0;
+
+    //     while(i <= mid && j <= ei) {
+    //         if(arr[i] < arr[j]) {
+    //             temp[k] = arr[i];
+    //             i++;
+    //         } else {
+    //             temp[k] = arr[j];
+    //             j++;
+    //         }
+    //         k++;
+    //     }
+    //     while(i <= mid) {
+    //         temp[k++] = arr[i++];
+    //     }
+        
+    //     while(j <= ei) {
+    //         temp[k++] = arr[j++];
+    //     }
+
+    //     for(k=0, i=si; k<temp.length; k++, i++) {
+    //         arr[i] = temp[k];
+    //     }
+    // }
+
+
     public static void printArr(int arr[]) {
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i]+" ");
         }
     }
-    public static void mergeSort(int arr[], int si, int ei) {
+    public static void quickSort(int arr[], int si, int ei) {
         // Base case
         if(si >= ei) {
             return;
         }
-        // Kaam
-        int mid = si + (ei-si)/2;
-        mergeSort(arr, si, mid);
-        mergeSort(arr, mid+1, ei);
-        merge(arr, si, mid, ei);
+        // kaam
+        int pIdx = partition(arr, si, ei);
+        quickSort(arr, si, pIdx-1);
+        quickSort(arr, pIdx+1, ei);
     }
-
-    public static void merge(int arr[], int si, int mid, int ei) {
-        int temp[] = new int[ei-si+1];
-        int i = si;
-        int j = mid+1;
-        int k = 0;
-
-        while(i <= mid && j <= ei) {
-            if(arr[i] < arr[j]) {
-                temp[k] = arr[i];
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si-1;
+        for(int j=si; j<ei; j++) {
+            if(arr[j] <= pivot) {
                 i++;
-            } else {
-                temp[k] = arr[j];
-                j++;
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
             }
-            k++;
         }
-        while(i <= mid) {
-            temp[k++] = arr[i++];
-        }
-        
-        while(j <= ei) {
-            temp[k++] = arr[j++];
-        }
-
-        for(k=0, i=si; k<temp.length; k++, i++) {
-            arr[i] = temp[k];
-        }
-}
+        i++;
+        // swap
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
     public static void main(String args[]) {
         int arr[] = {6, 3, 9, 5, 2, 8};
-        mergeSort(arr, 0, arr.length-1);
+        // mergeSort(arr, 0, arr.length-1);
+        quickSort(arr, 0, arr.length-1);
         printArr(arr);
     }
-
 }
