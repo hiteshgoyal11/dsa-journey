a51826d1d420cafa36266c5485bc885bcb020845
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-07 09:37:55 +0530

    Adds digits until a single digit is obtained added in DsaCllgBasics

---
+++ DsaCllgBasics.java
@@ -0,0 +1,20 @@
+public class DsaCllgBasics {
+    // 1. Reapeatedly adds digits until a single digit is obtained
+    // public static int addDigits(int num) {
+    //     if(num == 0) {
+    //         return 0;
+    //     }
+    //     if(num % 9 == 0) {
+    //         return 9;
+    //     } else {
+    //         return num % 9;
+    //     }
+    // }
+
+    // 
+
+    public static void main(String[] args) {
+        // int num = 987;
+        // System.out.println(addDigits(num));
+    }
+}
