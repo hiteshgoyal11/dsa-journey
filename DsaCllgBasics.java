59a8fac49e71b23c85c40d575f3dc32a0120198c
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-07 09:45:35 +0530

    count total digits present in a number problem added in DsaCllgBasics

--- DsaCllgBasics.java
+++ DsaCllgBasics.java
@@ -11,10 +11,25 @@ public class DsaCllgBasics {
     //     }
     // }
 
-    // 
+    // 2. count total digits present in a number
+    public static int countDigits(int num) {
+        int count = 0;
+        while(num > 0) {
+            count++;
+            num /= 10;
+        }
+        return count;
+    }
 
     public static void main(String[] args) {
+        // Problem 1
         // int num = 987;
         // System.out.println(addDigits(num));
+
+
+
+        // Problem 2
+        int num = 4589;
+        System.out.println(countDigits(num));
     }
 }
