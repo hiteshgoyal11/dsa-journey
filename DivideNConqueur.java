9665c95227ebd271fe78795694412acaef1a68ee
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-12 17:08:44 +0530

    Comment out example usage of search in DivideNConqueur.java

--- DivideNConqueur.java
+++ DivideNConqueur.java
@@ -134,9 +134,9 @@ public class DivideNConqueur {
 
 
         // Rotated Sorted Array
-        int arr[] = {4, 5, 6, 7, 0, 1, 2, -2};
-        int target = 0; //output -> 4
-        int tarIdx = search(arr, target, 0, arr.length-1);
-        System.out.println(tarIdx);
+        // int arr[] = {4, 5, 6, 7, 0, 1, 2, -2};
+        // int target = 0; //output -> 4
+        // int tarIdx = search(arr, target, 0, arr.length-1);
+        // System.out.println(tarIdx);
     }
 }
