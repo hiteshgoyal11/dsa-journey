fb6448de72238fe5933cf460c1f8695520a0f945
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-13 20:36:39 +0530

    Fix formatting for output comment in Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -61,13 +61,13 @@
 
 
 
-// What will be the output of the following code?
-//     public static void main(String args[]) {
-//         String str = "ShradhaDidi";
-//         String str1 = "ApnaCollege";
-//         String str2 = "ShradhaDidi";
-//         System.out.println(str.equals(str1) + " " + str.equals(str2));
-//     }
+    // What will be the output of the following code?
+    // public static void main(String args[]) {
+    //     String str = "ShradhaDidi";
+    //     String str1 = "ApnaCollege";
+    //     String str2 = "ShradhaDidi";
+    //     System.out.println(str.equals(str1) + " " + str.equals(str2));
+    // }
 
 
 
