3b711539766fd62ac200e8404ec95f1401ada2e2
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-20 17:43:53 +0530

    Implement isSafe method and update nQueens method for backtracking logic in BackTracking.java

--- BackTracking.java
+++ BackTracking.java
@@ -60,6 +60,28 @@ public  class BackTracking {
 
 
 
+    public static boolean isSafe(char board[][], int row, int col) {
+        // vertical up
+        for(int i=row-1; i>=0; i--) {
+            if(board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // diag left up
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // diag right up
+        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
     // N Queens Problem
     public static void nQueens(char board[][], int row) {
         // base
@@ -69,9 +91,11 @@ public  class BackTracking {
         }
         // column loop
         for(int j=0; j<board.length; j++) {
-            board[row][j] = 'Q';
-            nQueens(board, row+1); //function call
-            board[row][j] = 'x'; //backtracking step
+            if(isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row+1); //function call
+                board[row][j] = 'x'; //backtracking step
+            }
         }
     }
 
@@ -105,7 +129,7 @@ public  class BackTracking {
 
 
         // 
-        int n = 2;
+        int n = 5;
         char board[][] = new char[n][n];
         // initialize
         for(int i=0; i<n; i++) {
