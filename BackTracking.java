ce8464ffaf1b1f9e409e191e2ef7117de8e4c57c
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-20 16:36:53 +0530

    Comment out findSubsets method and update findPermutation method for clarity in BackTracking.java

--- BackTracking.java
+++ BackTracking.java
@@ -24,22 +24,38 @@ public  class BackTracking {
 
 
     // Find all subsets of a string using backtracking
-    public static void findSubsets(String str, String ans, int i) {
+    // public static void findSubsets(String str, String ans, int i) {
         // base case
-        if(i == str.length()) {
-            if(ans.length() == 0) {
-                System.out.println("null");
-            } else {
-                System.out.println(ans);
-            }
-            return;
-        }
+        // if(i == str.length()) {
+        //     if(ans.length() == 0) {
+        //         System.out.println("null");
+        //     } else {
+        //         System.out.println(ans);
+        //     }
+        //     return;
+        // }
         // Recursion
         // Yes choice
-        findSubsets(str, ans+str.charAt(i), i+1);
+        // findSubsets(str, ans+str.charAt(i), i+1);
         // No choice
-        findSubsets(str, ans, i+1);
+        // findSubsets(str, ans, i+1);
+    // }
 
+
+
+    // Find all permutations of a string using backtracking
+    public static void findPermutation(String str, String ans) {
+        // base case
+        if(str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        // recursion
+        for(int i=0; i<str.length(); i++) {
+            char curr = str.charAt(i);
+            String Newstr = str.substring(0, i) + str.substring(i+1);
+            findPermutation(Newstr, ans+curr);
+        }
     }
     public static void main(String args[]) {
 
@@ -50,7 +66,13 @@ public  class BackTracking {
 
 
         // Find all subsets of a string using backtracking
+        // String str = "abc";
+        // findSubsets(str, "", 0);
+        
+        
+
+        // Find all permutations of a string using backtracking
         String str = "abc";
-        findSubsets(str, "", 0);
+        findPermutation(str, "");
     }
 }
