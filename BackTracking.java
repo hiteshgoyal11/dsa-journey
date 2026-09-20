43462dd6be5fd437e35dbf8c9769c14b42ac8115
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-20 19:09:42 +0530

    Comment out N Queens methods and implement gridWays method for counting paths in a grid

--- BackTracking.java
+++ BackTracking.java
@@ -60,60 +60,75 @@ public  class BackTracking {
 
 
 
-    public static boolean isSafe(char board[][], int row, int col) {
-        // vertical up
-        for(int i=row-1; i>=0; i--) {
-            if(board[i][col] == 'Q') {
-                return false;
-            }
-        }
-        // diag left up
-        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--) {
-            if(board[i][j] == 'Q') {
-                return false;
-            }
-        }
-        // diag right up
-        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++) {
-            if(board[i][j] == 'Q') {
-                return false;
-            }
-        }
+    // public static boolean isSafe(char board[][], int row, int col) {
+    //     // vertical up
+    //     for(int i=row-1; i>=0; i--) {
+    //         if(board[i][col] == 'Q') {
+    //             return false;
+    //         }
+    //     }
+    //     // diag left up
+    //     for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--) {
+    //         if(board[i][j] == 'Q') {
+    //             return false;
+    //         }
+    //     }
+    //     // diag right up
+    //     for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++) {
+    //         if(board[i][j] == 'Q') {
+    //             return false;
+    //         }
+    //     }
 
-        return true;
-    }
-    // N Queens Problem
-    public static boolean nQueens(char board[][], int row) {
-        // base
-        if(row == board.length) {
-            // printBoard(board);
-            count++;
-            return true;
-        }
-        // column loop
-        for(int j=0; j<board.length; j++) {
-            if(isSafe(board, row, j)) {
-                board[row][j] = 'Q';
-                if(nQueens(board, row+1)) {
-                    return true;
-                } //function call
-                board[row][j] = 'x'; //backtracking step
-            }
-        }
-        return false;
-    }
+    //     return true;
+    // }
+    // N Queens Problem and Count the total ways to solve N Queens Problem and print 1 solution
+    // public static boolean nQueens(char board[][], int row) {
+    //     // base
+    //     if(row == board.length) {
+    //         // printBoard(board);
+    //         count++;
+    //         return true;
+    //     }
+    //     // column loop
+    //     for(int j=0; j<board.length; j++) {
+    //         if(isSafe(board, row, j)) {
+    //             board[row][j] = 'Q';
+    //             if(nQueens(board, row+1)) {
+    //                 return true;
+    //             } //function call
+    //             board[row][j] = 'x'; //backtracking step
+    //         }
+    //     }
+    //     return false;
+    // }
 
-    public static void printBoard(char board[][]) {
-        System.out.println("-------- chess board --------");
-        for(int i=0; i<board.length; i++) {
-            for(int j=0; j<board.length; j++) {
-                System.out.print(board[i][j]+ " ");
-            }
-            System.out.println();
-        }
-    }
+    // public static void printBoard(char board[][]) {
+    //     System.out.println("-------- chess board --------");
+    //     for(int i=0; i<board.length; i++) {
+    //         for(int j=0; j<board.length; j++) {
+    //             System.out.print(board[i][j]+ " ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
 
-    static int count = 0;
+    // static int count = 0;
+
+
+
+    // Grid Paths Problem
+    public static int gridWays(int i, int j, int n, int m) {
+        // base case
+        if(i == n-1 && j == m-1) { //condition for last cell
+            return 1;
+        } else if(i == n || j == m) { //boundary cross condition
+            return 0;
+        }
+        int w1 = gridWays(i+1, j, n , m);
+        int w2 = gridWays(i, j+1, n, m);
+        return w1+w2;
+    }
 
     public static void main(String args[]) {
 
@@ -135,21 +150,26 @@ public  class BackTracking {
 
 
 
-        // 
-        int n = 4;
-        char board[][] = new char[n][n];
+        // N Queens Problem and Count the total ways to solve N Queens Problem and print 1 solution
+        // int n = 4;
+        // char board[][] = new char[n][n];
         // initialize
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                board[i][j] = 'x';
-            } 
-        }
-        if(nQueens(board, 0)) {
-            System.out.println("Solution is possible");
-            printBoard(board);
-        } else {
-            System.out.println("Solution is not possible");
-        }
-        System.out.println("total ways to solve n queens = " +count);
+        // for(int i=0; i<n; i++) {
+        //     for(int j=0; j<n; j++) {
+        //         board[i][j] = 'x';
+        //     } 
+        // }
+        // if(nQueens(board, 0)) {
+        //     System.out.println("Solution is possible");
+        //     printBoard(board);
+        // } else {
+        //     System.out.println("Solution is not possible");
+        // }
+        // System.out.println("total ways to solve n queens = " +count);
+
+
+
+        int n = 3, m = 3;
+        System.out.println(gridWays(0, 0, n, m));
     }
 }
