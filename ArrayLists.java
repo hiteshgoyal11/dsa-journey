3920a3602f025f2cc8f725ca90424092ee794d05
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-22 22:57:47 +0530

    Refactor maximum element calculation in ArrayLists class for clarity

--- ArrayLists.java
+++ ArrayLists.java
@@ -54,14 +54,35 @@ public class ArrayLists {
 
 
         // print reverse of an arraylist
+        // list.add(1);
+        // list.add(2);
+        // list.add(3);
+        // list.add(4);
+        // list.add(5);
+        // for(int i=list.size()-1; i>=0; i--) {
+        //     System.out.print(list.get(i)+ " ");
+        // }
+        // System.out.println();
+
+
+        // print maximum
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         list.add(5);
-        for(int i=list.size()-1; i>=0; i--) {
-            System.out.print(list.get(i)+ " ");
+        // O(n) - time complexity
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<list.size(); i++) {
+            // if(max < list.get(i)) {
+            //     max = list.get(i);
+            // }
+
+            // or
+
+            max = Math.max(max, list.get(i));
         }
-        System.out.println();
+        System.out.println("max element = " +max);
+
     }
 }
