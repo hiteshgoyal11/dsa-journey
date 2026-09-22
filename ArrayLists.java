d9931de76b626f13d8e09b4e302a3a8e7c807f7d
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-22 22:23:44 +0530

    Add initial ArrayLists class with main method and example comments

---
+++ ArrayLists.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArrayLists {
+    public static void main(String args[]) {
+
+        // example to show how to create an ArrayList
+        // ArrayList<Integer> list = new ArrayList<>();
+        // ArrayList<String> list2 = new ArrayList<>();
+        // ArrayList<Boolean> list3 = new ArrayList<>();
+    }
+}
