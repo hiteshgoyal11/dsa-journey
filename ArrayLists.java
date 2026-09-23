5cd3a26d9184635914facd9b301be82ea169e95e
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-23 21:22:22 +0530

    Refactor ArrayLists class: comment out swap method and add example usage for sorting

--- ArrayLists.java
+++ ArrayLists.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
-
+import java.util.Collections;
 public class ArrayLists {
 
     // Swap 2 numbers
-    public static void swap(ArrayList<Integer> list, int idx1, int idx2) {
-        int temp = list.get(idx1);
-        list.set(idx1, list.get(idx2));
-        list.set(idx2, temp);
-    }
+    // public static void swap(ArrayList<Integer> list, int idx1, int idx2) {
+    //     int temp = list.get(idx1);
+    //     list.set(idx1, list.get(idx2));
+    //     list.set(idx2, temp);
+    // }
     public static void main(String args[]) {
 
         // example to show how to create an ArrayList
@@ -93,16 +93,29 @@ public class ArrayLists {
 
 
         // Swap 2 numbers
+        // ArrayList<Integer> list = new ArrayList<>();
+        // list.add(2);
+        // list.add(5);
+        // list.add(9);
+        // list.add(3);
+        // list.add(6);
+
+        // int idx1 = 1, idx2 = 3;
+        // System.out.println(list);
+        // swap(list, idx1, idx2);
+        // System.out.println(list);
+
+
+
+        // Sorting on ArrayList
         ArrayList<Integer> list = new ArrayList<>();
         list.add(2);
         list.add(5);
         list.add(9);
         list.add(3);
         list.add(6);
-
-        int idx1 = 1, idx2 = 3;
         System.out.println(list);
-        swap(list, idx1, idx2);
+        Collections.sort(list);
         System.out.println(list);
     }
 }
