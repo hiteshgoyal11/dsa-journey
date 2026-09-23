534ac87399bdffdbd9fd6f517eb0195430780524
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-23 21:15:12 +0530

    Implement swap method and update main method with example usage in ArrayLists class

--- ArrayLists.java
+++ ArrayLists.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
 
 public class ArrayLists {
+
+    // Swap 2 numbers
+    public static void swap(ArrayList<Integer> list, int idx1, int idx2) {
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+    }
     public static void main(String args[]) {
 
         // example to show how to create an ArrayList
-        ArrayList<Integer> list = new ArrayList<>();
+        // ArrayList<Integer> list = new ArrayList<>();
         // ArrayList<String> list2 = new ArrayList<>();
         // ArrayList<Boolean> list3 = new ArrayList<>();
 
@@ -66,23 +73,36 @@ public class ArrayLists {
 
 
         // print maximum
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
+        // list.add(1);
+        // list.add(2);
+        // list.add(3);
+        // list.add(4);
+        // list.add(5);
         // O(n) - time complexity
-        int max = Integer.MIN_VALUE;
-        for(int i=0; i<list.size(); i++) {
+        // int max = Integer.MIN_VALUE;
+        // for(int i=0; i<list.size(); i++) {
             // if(max < list.get(i)) {
             //     max = list.get(i);
             // }
 
             // or
 
-            max = Math.max(max, list.get(i));
-        }
-        System.out.println("max element = " +max);
+            // max = Math.max(max, list.get(i));
+        // }
+        // System.out.println("max element = " +max);
 
+
+        // Swap 2 numbers
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+
+        int idx1 = 1, idx2 = 3;
+        System.out.println(list);
+        swap(list, idx1, idx2);
+        System.out.println(list);
     }
 }
