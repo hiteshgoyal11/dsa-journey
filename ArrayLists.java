7e3e99a492f2b0c1d2c42e8a04c94d691a2f0ccc
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-24 21:02:37 +0530

    Refactor pairSum1 method: replace brute force approach with optimized two-pointer technique

--- ArrayLists.java
+++ ArrayLists.java
@@ -50,16 +50,39 @@ public class ArrayLists {
 
 
     // pair sum-1 Brute Force 0(n2)
+    // public static boolean pairSum1(ArrayList<Integer> list, int target) {
+    //     for(int i=0; i<list.size(); i++) {
+    //         for(int j=i+1; j<list.size(); j++) {
+    //             if(list.get(i)+list.get(j) == target) {
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+
+    // pair sum-1 2 pointer approach 0(n)
     public static boolean pairSum1(ArrayList<Integer> list, int target) {
-        for(int i=0; i<list.size(); i++) {
-            for(int j=i+1; j<list.size(); j++) {
-                if(list.get(i)+list.get(j) == target) {
-                    return true;
-                }
+        int lp = 0;
+        int rp = list.size()-1;
+        while(lp != rp) {
+            // case 1
+            if(list.get(lp)+list.get(rp) == target) {
+                return true;
+            }
+
+            // case 2
+            if(list.get(lp)+list.get(rp) < target) {
+                lp++;
+            } else {
+                // case 3
+                rp--;
             }
         }
         return false;
     }
+
     public static void main(String args[]) {
 
         // example to show how to create an ArrayList
