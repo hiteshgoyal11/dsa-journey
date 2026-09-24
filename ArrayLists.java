49916bdec835fd8677f0c9a1763f3a5b6a20232b
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-24 20:40:31 +0530

    add pairSum1 method for target sum check in Brute force

--- ArrayLists.java
+++ ArrayLists.java
@@ -26,26 +26,39 @@ public class ArrayLists {
 
 
     // Container with most water - Optimized Approach 0(n)
-    public static int storeWater(ArrayList<Integer> height) {
-        int maxWater = 0;
-        int lp = 0;
-        int rp = height.size()-1;
+    // public static int storeWater(ArrayList<Integer> height) {
+    //     int maxWater = 0;
+    //     int lp = 0;
+    //     int rp = height.size()-1;
 
-        while(lp < rp) {
-            // calculate water area
-            int ht = Math.min(height.get(lp), height.get(rp));
-            int width = rp-lp;
-            int currWater = ht * width;
-            maxWater = Math.max(maxWater, currWater);
+    //     while(lp < rp) {
+    //         // calculate water area
+    //         int ht = Math.min(height.get(lp), height.get(rp));
+    //         int width = rp-lp;
+    //         int currWater = ht * width;
+    //         maxWater = Math.max(maxWater, currWater);
 
-            // update pointer
-            if(height.get(lp) < height.get(rp)) {
-                lp++;
-            } else {
-                 rp--;
+    //         // update pointer
+    //         if(height.get(lp) < height.get(rp)) {
+    //             lp++;
+    //         } else {
+    //              rp--;
+    //         }
+    //     }
+    //     return maxWater;
+    // }
+
+
+    // pair sum-1 Brute Force 0(n2)
+    public static boolean pairSum1(ArrayList<Integer> list, int target) {
+        for(int i=0; i<list.size(); i++) {
+            for(int j=i+1; j<list.size(); j++) {
+                if(list.get(i)+list.get(j) == target) {
+                    return true;
+                }
             }
         }
-        return maxWater;
+        return false;
     }
     public static void main(String args[]) {
 
@@ -210,17 +223,29 @@ public class ArrayLists {
         // }
 
 
-        // Container with most water - Brute Force 0(n2)
-        ArrayList<Integer> height = new ArrayList<>();
-        height.add(1);
-        height.add(8);
-        height.add(6);
-        height.add(2);
-        height.add(5);
-        height.add(4);
-        height.add(8);
-        height.add(3);
-        height.add(7);
-        System.out.println(storeWater(height));
+        // Container with most water
+        // ArrayList<Integer> height = new ArrayList<>();
+        // height.add(1);
+        // height.add(8);
+        // height.add(6);
+        // height.add(2);
+        // height.add(5);
+        // height.add(4);
+        // height.add(8);
+        // height.add(3);
+        // height.add(7);
+        // System.out.println(storeWater(height));
+
+
+        // pair sum-1
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target = 5;
+        System.out.println(pairSum1(list, target));
     }
 }
