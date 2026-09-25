5000092a755d5deb3d2a7ba31a6537416634b8bc
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-25 21:09:23 +0530

    Add LinkedList class with methods to add nodes and print the list

--- ArrayLists.java
+++ ArrayLists.java
@@ -85,35 +85,35 @@ public class ArrayLists {
 
 
     // pair sum-2 0(n)
-    public static boolean pairSum2(ArrayList<Integer> list, int target) {
-        int bp = -1;
-        int n = list.size();
-        for(int i=0; i<list.size(); i++) {
-            if(list.get(i) > list.get(i+1)) { //breaking point
-                bp = 1;
-                break;
-            }
-        }
+    // public static boolean pairSum2(ArrayList<Integer> list, int target) {
+    //     int bp = -1;
+    //     int n = list.size();
+    //     for(int i=0; i<list.size(); i++) {
+    //         if(list.get(i) > list.get(i+1)) { //breaking point
+    //             bp = 1;
+    //             break;
+    //         }
+    //     }
 
-        int lp = bp+1; //smallest
-        int rp = bp; //largest
+    //     int lp = bp+1; //smallest
+    //     int rp = bp; //largest
 
-        while(lp != rp) {
-            // case 1
-            if(list.get(lp) + list.get(rp) == target) {
-                return true;
-            }
+    //     while(lp != rp) {
+    //         // case 1
+    //         if(list.get(lp) + list.get(rp) == target) {
+    //             return true;
+    //         }
 
-            // case 2
-            if(list.get(lp) + list.get(rp) < target) {
-                lp = (lp+1)%n;
-            } else {
-                // case 3
-                rp = (n+rp-1)%n;
-            }
-        }
-        return false;
-    }
+    //         // case 2
+    //         if(list.get(lp) + list.get(rp) < target) {
+    //             lp = (lp+1)%n;
+    //         } else {
+    //             // case 3
+    //             rp = (n+rp-1)%n;
+    //         }
+    //     }
+    //     return false;
+    // }
 
     public static void main(String args[]) {
 
@@ -305,14 +305,14 @@ public class ArrayLists {
 
 
         // pair sum-2 0(n)
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(11);
-        list.add(15);
-        list.add(6);
-        list.add(8);
-        list.add(9);
-        list.add(10);
-        int target = 16;
-        System.out.println(pairSum2(list, target));
+        // ArrayList<Integer> list = new ArrayList<>();
+        // list.add(11);
+        // list.add(15);
+        // list.add(6);
+        // list.add(8);
+        // list.add(9);
+        // list.add(10);
+        // int target = 16;
+        // System.out.println(pairSum2(list, target));
     }
 }
