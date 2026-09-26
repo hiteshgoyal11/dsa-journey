22dceb42f01f02e178cd8fe0025b781883100149
hiteshgoyal11 <goyalhitesh425@gmail.com>
2026-09-26 10:25:50 +0530

    Comment out Node class and its methods in LinkedList for future reference

--- LinkedList.java
+++ LinkedList.java
@@ -1,49 +1,49 @@
 public class LinkedList {
 
-    public static class Node {
-        int data;
-        Node next;
+    // public static class Node {
+    //     int data;
+    //     Node next;
 
-        public Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-    public static Node head;
-    public static Node tail;
+    //     public Node(int data) {
+    //         this.data = data;
+    //         this.next = null;
+    //     }
+    // }
+    // public static Node head;
+    // public static Node tail;
 
-    public void addFirst(int data) {
-        // step1 = create new node
-        Node newNode = new Node(data);
-        if(head == null) {
-            head = tail = newNode;
-            return;
-        }
-        // step2 - newNode next = head
-        newNode.next = head; //link
+    // public void addFirst(int data) {
+    //     // step1 = create new node
+    //     Node newNode = new Node(data);
+    //     if(head == null) {
+    //         head = tail = newNode;
+    //         return;
+    //     }
+    //     // step2 - newNode next = head
+    //     newNode.next = head; //link
 
-        // step3 - head = newNode
-        head = newNode;
-    }
+    //     // step3 - head = newNode
+    //     head = newNode;
+    // }
 
-    public void addLast(int data) {
-        Node newNode = new Node(data);
-        if(head == null) {
-            head = tail = newNode;
-            return;
-        }
-        tail.next = newNode;
-        tail = newNode;
-    }
+    // public void addLast(int data) {
+    //     Node newNode = new Node(data);
+    //     if(head == null) {
+    //         head = tail = newNode;
+    //         return;
+    //     }
+    //     tail.next = newNode;
+    //     tail = newNode;
+    // }
 
-    public void print() { //0(n)
-        Node temp = head;
-        while(temp != null) {
-            System.out.print(temp.data+ "->");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
+    // public void print() { //0(n)
+    //     Node temp = head;
+    //     while(temp != null) {
+    //         System.out.print(temp.data+ "->");
+    //         temp = temp.next;
+    //     }
+    //     System.out.println("null");
+    // }
     public static void main(String args[]) {
         LinkedList ll = new LinkedList();
         ll.print();
